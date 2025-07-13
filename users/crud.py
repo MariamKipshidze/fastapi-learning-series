2441d8c07a073914081f95a3b2fd6ddcca22fa76
mariam.kipshidze <mariam.kipshidze@swisscapital.ge>
2025-07-13 16:06:59 +0400

    Fixed users crud

--- users/crud.py
+++ users/crud.py
@@ -1,14 +1,14 @@
+from sqlalchemy.orm import Session
 from users import models, schemas
-from database import SessionLocal
 
-db = SessionLocal()
 
-def create_user(user: schemas.UserCreate):
+def create_user(user: schemas.UserCreate, db: Session):
     db_user = models.User(**user.model_dump())
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
-def get_users():
+
+def get_users(db: Session):
     return db.query(models.User).all()

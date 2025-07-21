8b94b2076d7047b14b3af896f49db3c37bc6268d
MariamKipshidze <mari.kifshidze@gmail.com>
2025-07-21 17:55:31 +0400

    Fixed pep8

--- users/routes.py
+++ users/routes.py
@@ -6,11 +6,12 @@ from users.crud import create_user, get_users
 
 router = APIRouter()
 
+
 @router.post("/", response_model=UserOut)
 def create_user_route(user: UserCreate):
     return create_user(user)
 
+
 @router.get("/", response_model=List[UserOut])
 def get_users_route():
     return get_users()
-

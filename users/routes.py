596a08382e8c1e7ad1a8c81aef4643b199bb8261
MariamKipshidze <mari.kifshidze@gmail.com>
2025-07-21 17:50:33 +0400

    Revert "Fixed pep8"
    This reverts commit b9e6e0b25f9729d8e70c5a8ff896c6a9d6739881.

--- users/routes.py
+++ users/routes.py
@@ -6,12 +6,11 @@ from users.crud import create_user, get_users
 
 router = APIRouter()
 
-
 @router.post("/", response_model=UserOut)
 def create_user_route(user: UserCreate):
     return create_user(user)
 
-
 @router.get("/", response_model=List[UserOut])
 def get_users_route():
     return get_users()
+
